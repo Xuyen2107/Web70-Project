e39a255bea37e525a080e7689105ea9d8ba8b78b
Xuyen <hoangxuyenpham30@gmail.com>
2023-08-20 21:15:57 +0700

    project

--- server/src/configs/db.config.js
+++ server/src/configs/db.config.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.REACT_APP_MONGO_URI;
 console.log(MONGO_URI);
 
 export const connectToDatabase = async () => {
    try {
-      const connection = await mongoose.connect(
-         "mongodb+srv://social-app-admin:admin123@social-app.gy8vwug.mongodb.net/?retryWrites=true&w=majority"
-      );
+      const connection = await mongoose.connect(MONGO_URI);
       console.log(`Database is connected at ${connection.connection.host}`);
    } catch (error) {
       console.log(error);
